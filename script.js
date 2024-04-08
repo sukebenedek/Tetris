be3b86a3a6e43a8e1a856571f2e790bc6848d7d5
sukebenedek <suke.benedek@students.jedlik.eu>
2024-04-08 16:59:02 +0200

    jobra balra

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ ctx.fillRect(0, 0, width, height)
 let g = []
 let next = []
 let pause = false
+let fps = 2
 let putNext = true
 for (let i = 0; i < maxInCols; i++) {
     g.push([])
@@ -35,7 +36,7 @@ function drawFrame(){
                 ctx.fillStyle = "white"
             }
             else{
-                console.log(`Nínó`);
+                console.log(`Nínó: ${g[i][j].st}`);
             }
             ctx.fillRect(pencil.posx, pencil.posy, particleSize - border, particleSize - border)
             pencil.posx += particleSize
@@ -71,7 +72,6 @@ function move(){
                 stopControllables()
             }
             else if(i != maxInCols - 1 && cell.st == b && g[i+1][j].st == e && cell.shouldMove && !cell.didMove){
-                console.log(g[i][j]);
                 changeST(g[i+1][j], g[i][j])
             }
         }
@@ -101,16 +101,17 @@ function stopControllables(){
 }
 
 function changeST(c1, c2){
-    let temp = {st: c2.st, shouldMove: c2.shouldMove, didMove: c2.didMove, controllable: c2.controllable}
+    let temp = {st: c2.st, shouldMove: c2.shouldMove, controllable: c2.controllable}
     c2.st = c1.st
     c2.shouldMove = c1.shouldMove
-    c2.didMove = true
     c2.controllable = c1.controllable
-
+    
     c1.st = temp.st
     c1.shouldMove = temp.shouldMove
-    c1.didMove = true
     c1.controllable = temp.controllable
+
+    c2.didMove = true
+    c1.didMove = true
 }
 
 
@@ -130,6 +131,59 @@ function changeST(c1, c2){
 
 
 
+document.querySelector("body").addEventListener("keydown", function(event) {
+    let control = []
+    if(event.key == "ArrowLeft" || event.key.toLowerCase() == "a"){
+        for (let i = 0; i < g.length; i++) {
+            for (let j = 0; j < g[i].length; j++) {
+                if(g[i][j].controllable){
+                    control.push({cell: g[i][j], x: j, y: i})
+                }
+            }
+        }
+
+        isPossible = true
+        for (let i = 0; i < control.length; i++) {
+            let neighbour = g[control[i].y][control[i].x-1]
+            if(!(isPossible && control[i].x != 0 && (neighbour.st == e || neighbour.controllable))){
+                isPossible = false
+            }
+        }
+
+        console.log(isPossible);
+        if(isPossible){
+            for (let i = 0; i < control.length; i++) {
+                changeST(g[control[i].y][control[i].x-1], control[i].cell)
+            }
+            drawFrame()
+        }
+    }
+
+    if(event.key == "ArrowRight" || event.key.toLowerCase() == "d"){
+        for (let i = 0; i < g.length; i++) {
+            for (let j = 0; j < g[i].length; j++) {
+                if(g[i][j].controllable){
+                    control.push({cell: g[i][j], x: j, y: i})
+                }
+            }
+        }
+
+        isPossible = true
+        for (let i = 0; i < control.length; i++) {
+            let neighbour = g[control[i].y][control[i].x+1]
+            if(!(isPossible && control[i].x != maxInRows-1 && (neighbour.st == e || neighbour.controllable))){
+                isPossible = false
+            }
+        }
+
+        console.log(isPossible);
+        if(isPossible){
+            for (let i = control.length-1; i >= 0; i--) {
+                changeST(g[control[i].y][control[i].x+1], control[i].cell)
+            }
+            drawFrame()
+        }
+    }
 
 
 
@@ -138,30 +192,11 @@ function changeST(c1, c2){
 
 
 
-
-
-
-
-
-
-
-
-document.querySelector("body").addEventListener("keydown", function(e) {
-    if(e.key == "ArrowLeft"){
-        let control = g.flatMap(row => row.filter(obj => obj.controllable === true));
-
-
-        console.log(control);
+    if(event.key == "ArrowDown" || event.key.toLowerCase() == "s"){
+        move()
     }
 }) 
 
-
-
-
-
-
-
-
-
 document.querySelector("body").addEventListener("click", move)
 drawFrame()
+let moveInterval = setInterval(move, 1000/fps)
