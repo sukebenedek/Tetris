8247a65153b209c9dc112dbd8226de2e22d3542b
[Pap László] <[pap.laszlo@students.jedlik.eu]>
2024-04-23 13:33:16 +0200

    script jsbe belemásoltam dolgokat, minden más nem működött xdddd

--- script.js
+++ script.js
@@ -552,8 +552,37 @@ function PP(){
     }
 }
 
+select = document.querySelector("#theme");
+webBody =  document.querySelector("body");
+select.addEventListener('change', changeTheme);
 
+function changeTheme(){
+    if (select.value == "dark"){
+        webBody.style.backgroundColor = "black"
+        webBody.style.color = "white"
+        webBody.style.backgroundImage = "none";
+        webBody.style.fontFamily = "Arial"
 
+    }
+    if (select.value == "light"){
+        webBody.style.backgroundColor = "bisque"
+        webBody.style.color = "darkslategray"
+        webBody.style.backgroundImage = "none";
+        webBody.style.fontFamily = "cursive"
+    }
+    if (select.value == "egypt"){
+        webBody.style.backgroundImage = `url("imidzs/piramisok.jpg")`;
+        webBody.style.backgroundRepeat = "no-repeat";
+        webBody.style.backgroundSize = "cover"
+        webBody.style.color = "blanchedalmond"
+        webBody.style.fontFamily = "'Times New Roman', Times, serif"
+    }
+    if (select.value == "cyber"){
+        webBody.style.backgroundImage = `url("imidzs/cyber.jpg")`;
+        webBody.style.backgroundRepeat = "no-repeat";
+        webBody.style.backgroundSize = "cover"
+        webBody.style.color = "cyan"
+        webBody.style.fontFamily = "'Courier New', Courier, monospace"
 
-
-
+    }
+}
